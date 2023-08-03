8c033e056ed3862c4085d24fe97ca1cee1df1a8d
lash <dev@holbrook.no>
2023-08-03 15:45:46 +0100

    Add access guard to setLimitFor in index contract version

--- solidity/LimiterIndex.sol
+++ solidity/LimiterIndex.sol
@@ -35,6 +35,7 @@ contract LimiterTokenRegistry {
 	}
 
 	function setLimitFor(address _token, address _holder, uint256 _value) public {
+		require(msg.sender == owner || msg.sender == _holder, 'ERR_AXX');
 		limiter.setLimitFor(_token, _holder, _value);
 	}
 
